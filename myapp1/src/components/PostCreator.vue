85fac3f4940ef4c12cdc5605d25dff5ecfa53bfb
noobthaibot <dgugik@ukr.net>
2022-02-02 16:31:06 +0200

    tried to made post creator

---
+++ myapp1/src/components/PostCreator.vue
@@ -0,0 +1,38 @@
+<template>
+ <div>
+    <my-input
+    label="Название"
+    :modelValue="title"
+    @update="onUpdateTitle"
+    v-model="title"
+  />
+  <my-textarea
+    label="Введите текст"
+    :modelValue="text"
+    @update="onUpdateText"
+    v-model="text"
+  />
+ </div>
+</template>
+
+<script>
+import MyInput from './ui/MyInput.vue'
+import MyTextarea from './ui/MyTextarea.vue'
+export default {
+  components: { MyInput, MyTextarea },
+  data () {
+    return {
+      title: '',
+      text: ''
+    }
+  },
+  methods: {
+    onUpdateTitle (modelValue) {
+      this.title = modelValue
+    },
+    onUpdateText (modelValue) {
+      this.text = modelValue
+    }
+  }
+}
+</script>
--- myapp1/src/views/Settings.vue
+++ myapp1/src/views/Settings.vue
@@ -1,5 +1,6 @@
 <template>
-<div><posts-form v-for="item in news"
+<div>
+  <posts-form v-for="item in news"
       :key="item.id"
       :title="item.title"
       :image="item.image"
@@ -8,12 +9,19 @@
       :is-open="item.isOpen">
       </posts-form>
         <div class="counter">
-      <div> Общее количество постов: {{ news.length }}</div>
-    </div></div>
+      <div v-if="$store.state.posts.length !== 0"> Общее количество постов: {{ news.length }}</div>
+      <div v-show="$store.state.posts.length === 0">
+        <div>Постов нет,создайте новый</div>
+        <post-creator></post-creator>
+        <button @click="submitPost">Создать</button>
+    </div>
+  </div>
+   </div>
 </template>
 
 <script>
 import PostsForm from '../components/PostsForm.vue'
+import PostCreator from '../components/PostCreator.vue'
 export default {
   data () {
     return {
@@ -27,7 +35,13 @@ export default {
     }
   },
   components: {
-    'posts-form': PostsForm
+    'posts-form': PostsForm,
+    'post-creator': PostCreator
+  },
+  methods: {
+    submitPost (modelValue) {
+      this.$store.state.posts.push(modelValue)
+    }
   }
 }
 </script>
